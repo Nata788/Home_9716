57ad42b080856477e5ba890d30fc7b6077fd45ab
Nata788 <Nata788@gmail.com>
2017-06-26 18:37:07 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,29 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    debugger
   end
 
   def new
+    @user = User.new
   end
 
-# Before filters
-
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User #{@user.email} is now in your database!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def index
+    @user = User.all
   end
 
 end
